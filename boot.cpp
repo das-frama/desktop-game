b40201678da63d3c2f82c25dd87c6fef4fd377e8
Andrey Galaktionov <das.frama@gmail.com>
2021-07-14 17:09:46 +0300

    Немного доработал boot.cpp

--- boot.cpp
+++ boot.cpp
@@ -3,57 +3,61 @@
 struct Boot_State {
 	bool is_initialized = false;
 	bool is_printing = true;
-	int printend = 0;
+	int current_message = 0;
 	float acc;
 } boot_state;
 
 struct Boot_Message {
 	const char* text = nullptr;
+	int time = 1000;
 	int delay = 1000;
 	bool is_printed = false;
 };
 
 Boot_Message boot_messages[] = {
-	{"Загрузка первичных драйверов", 5000},
-	{"Загрузка основных компонентов", 7000},
-	{"Проверка подписи", 12000},
+	{"Загрузка первичных драйверов", 1500, 1000},
+	{"Загрузка основных компонентов", 1000, 1000},
+	{"Проверка подписи", 2600, 1000},
+	{"Завершено...", 0, 0},
 };
-
+int messages_count = sizeof(boot_messages) / sizeof(boot_messages[0]);
 
 inline char*
-dots(const Boot_Message &message) {
+dots(const Boot_Message& message) {
 	u8 count = 0;
 	if (message.is_printed) {
-		count = message.delay / DOTS_SPEED;
+		count = message.time / DOTS_SPEED;
 	} else {
-		count = (int) boot_state.acc / DOTS_SPEED;
+		count = (int)boot_state.acc / DOTS_SPEED;
 	}
 
 	auto buf = (char*)malloc((count + 1) * sizeof(char));
 	for (int i = 0; i < count; i++) {
 		*(buf + i) = '.';
 	}
-	*(buf+count) = '\0';
+	*(buf + count) = '\0';
 
 	return buf;
 }
 
-
 internal void
 update_boot(f32 dt) {
 	// Accumalte dt.
-	{
+	if (boot_state.is_printing) {
 		boot_state.acc += dt;
 
-		if (boot_state.acc >= boot_messages[boot_state.printend].delay) {
-			boot_state.acc = 0;
-			boot_messages[boot_state.printend].is_printed = true;
-			boot_state.printend += 1;
+		auto message = &boot_messages[boot_state.current_message];
+		if (boot_state.acc >= message->time) {
+			message->is_printed = true;
+			if (boot_state.acc >= message->time + message->delay) {
+				boot_state.acc = 0;
+				boot_state.current_message += 1;
+			}
 		}
 
-		if (boot_state.printend >= 3) {
-			boot_state.printend = 2;
-			//boot_state.is_printing = false;
+		if (boot_state.current_message >= messages_count) {
+			boot_state.current_message = messages_count - 1;
+			boot_state.is_printing = false;
 			//boot_state.is_initialized = true;
 		}
 	}
@@ -62,10 +66,10 @@ update_boot(f32 dt) {
 	{
 		clear_screen(SDL_Color{ 0x0, 0x0, 0x0 });
 		SDL_Color white = { 0xff, 0xff, 0xff };
-		for (int i = 0; i < boot_state.printend + 1; i++) {
-			char buf[128];
+		for (int i = 0; i < boot_state.current_message + 1; i++) {
+			char buf[255];
 			sprintf_s(buf, "%s%s", boot_messages[i].text, dots(boot_messages[i]));
-			draw_text(buf, desktop_font, 15, 15 * i + (i * 25), white);
+			draw_text(buf, desktop_font, 15, 15 * i + (i * 15), white);
 		}
 	}
 }
